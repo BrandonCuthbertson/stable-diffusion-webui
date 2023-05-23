16b941383a2aec93f811764984e335158557ba89
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2023-05-23 12:53:54 -0600

    For back up

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
-
-set PYTHON=
+git checkout v1.1.0
+set PYTHON="C:\Users\brand\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --no-gradio-queue --api --cors-allow-origins=http://www.painthua.com --no-half
 
 call webui.bat
