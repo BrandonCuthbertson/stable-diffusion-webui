4d9262307291d03391bdd3cc366772c5f9e78691
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2023-05-23 13:14:41 -0600

    so it doesnt auto to head

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
-git checkout v1.1.0
+git switch -
 set PYTHON="C:\Users\brand\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --no-gradio-queue --api --cors-allow-origins=http://www.painthua.com --no-half
+set COMMANDLINE_ARGS= --api --cors-allow-origins=http://www.painthua.com --no-half
 
 call webui.bat
